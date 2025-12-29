25e387c35f63de17ed9cda38afecaed205b83c8d
Dmytro Spodarets <dmitry@spodarets.com>
2025-12-28 18:14:23 -0800

    v0.0.65

--- infra/envs/prod/app-runner.tf
+++ infra/envs/prod/app-runner.tf
@@ -9,7 +9,7 @@ resource "aws_apprunner_service" "surefilter" {
     }
 
     image_repository {
-      image_identifier      = "${aws_ecr_repository.surefilter.repository_url}:v0.0.64"
+      image_identifier      = "${aws_ecr_repository.surefilter.repository_url}:v0.0.65"
       image_repository_type = "ECR"
       image_configuration {
         port = "3000"
